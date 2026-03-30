f4c781593d044c2f0a4d14dcb80250c197f5fadb
Ubuntu <ubuntu@ip-172-31-3-239.ec2.internal>
2026-03-30 12:30:54 +0000

    add volumes and update redis host

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from redis import Redis
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6337)
+redis = Redis(host='my-database', port=6379)
 
 @app.route('/')
 def hello():
